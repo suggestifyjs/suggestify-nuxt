8ff7d11c447ffc84491c839d92617b0c626bc7f7
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2021-12-27 00:02:10 +0800

    WIP added helper functions

--- src/suggestify.ts
+++ src/suggestify.ts
@@ -1,11 +1,63 @@
-import { defineComponent, h, PropType, ref } from 'vue';
+import { defineComponent, h, PropType, ref, Ref } from 'vue';
 import { nanoid } from 'nanoid';
+import { $fetch } from 'ohmyfetch';
+
+// -------------------------------------------------------------
+// Typing
+// -------------------------------------------------------------
 
 export interface Translations {
 	placeholder?: string;
 	buttonSubmit?: string;
 	buttonCancel?: string;
 }
+
+export interface Event {
+	value: string;
+	success: 'HIT' | 'MISS';
+}
+
+export interface Translations {
+	suggestions: string;
+	results: string;
+}
+
+export interface Cache {
+	[key: string]: Result;
+}
+
+export interface Result {
+	type: 'results' | 'suggestions' | 'empty';
+	items: string[];
+	time: number;
+}
+
+// -------------------------------------------------------------
+// Helpers
+// -------------------------------------------------------------
+
+const sanitize = (string: string) => {
+	const map: { [key: string]: string } = {
+		'&': '&amp;',
+		'<': '&lt;',
+		'>': '&gt;',
+		'"': '&quot;',
+		"'": '&#x27;',
+		'`': '&grave;',
+		'/': '&#x2F;',
+	};
+	const reg = /[&<>"'/`]/gi;
+	return string.replace(reg, (match) => map[match]);
+};
+
+const switchFn =
+	(lookupObject: any, defaultCase = '_default') =>
+	(expression: string) =>
+		(lookupObject[expression] || lookupObject[defaultCase])();
+
+// -------------------------------------------------------------
+// Component
+// -------------------------------------------------------------
 
 export const Suggestify = defineComponent({
 	name: 'Suggestify',
@@ -17,9 +69,30 @@ export const Suggestify = defineComponent({
 		t: {
 			type: Object as PropType<Translations>,
 		},
+		url: {
+			type: String,
+			default: '?q=',
+		},
+		engine: {
+			type: String,
+			default: '',
+		},
+		blur: {
+			type: Boolean,
+			default: true,
+		},
+		instant: {
+			type: Boolean,
+			default: false,
+		},
+		event: {
+			type: Boolean,
+			default: false,
+		},
 	},
 	setup: (props, { slots }) => {
 		const id = nanoid(6);
+		// const timeout: number | any = 250;
 		const t = {
 			placeholder: 'Search...',
 			buttonSubmit: 'Search',
@@ -27,8 +100,76 @@ export const Suggestify = defineComponent({
 			...props.t,
 		};
 
-		const expanded = ref(false);
+		const input = ref();
 
+		// const selectedIndex = ref(-1);
+		const searchInput = ref('');
+		const cache: Ref<Cache> = ref({});
+		const expanded = ref(false);
+		const results: Ref<Result[]> = ref([]);
+
+		// ------------------------------------------------------------
+		//  Functions
+		// ------------------------------------------------------------
+		const request = async (search: string | null): Promise<Result> => {
+			const query = search ? search : null;
+			const cacheKey = JSON.stringify(query);
+			if (cache.value[cacheKey]) return cache.value[cacheKey];
+
+			try {
+				const { data } = await $fetch(`${props.engine}`, {
+					params: {
+						q: query,
+					},
+				});
+
+				cache.value[cacheKey] = data;
+				return data;
+			} catch (error: any) {
+				throw new Error(error);
+			}
+		};
+
+		const keyHandler = ({ key }: KeyboardEvent): void => {
+			const cases = {
+				Enter: directSearch,
+				Escape: deleteResultList,
+				ArrowUp: selectItemUp,
+				ArrowDown: selectItemDown,
+				_default: () => null,
+			};
+			const keySwitch = switchFn(cases, '_default');
+
+			keySwitch(key);
+		};
+
+		const clearInput = (): void => {
+			searchInput.value = '';
+			input.value = '';
+
+			deleteResultList();
+		};
+
+		const blurHandler = (): void => {
+			setTimeout(() => {
+				deleteResultList();
+			}, 100);
+		};
+
+		const inputSelected = async (): Promise<void> => {
+			try {
+				const response = await request(searchInput.value);
+
+				deleteResultList();
+				createResultList(response);
+			} catch (error: any) {
+				throw new Error(error);
+			}
+		};
+
+		// ------------------------------------------------------------
+		//  Render
+		// ------------------------------------------------------------
 		const icon = h('i', {
 			class: `${props.class}-icon`,
 			role: 'presentation',
@@ -36,9 +177,14 @@ export const Suggestify = defineComponent({
 			'aria-hidden': 'true',
 		});
 
-		const results: string[] = [];
+		// list item
+		const item = (item: any) =>
+			h('li', {
+				class: `${item.class}`,
+			});
 
 		return () =>
+			// root
 			h(
 				'div',
 				{
@@ -46,7 +192,9 @@ export const Suggestify = defineComponent({
 					role: 'search',
 				},
 				[
+					// input field
 					h('input', {
+						ref: input,
 						placeholder: t.placeholder,
 						'aria-label': t.placeholder,
 						class: `${props.class}-input`,
@@ -59,7 +207,13 @@ export const Suggestify = defineComponent({
 						'aria-haspopup': 'listbox',
 						'aria-expanded': expanded.value,
 						'aria-owns': `${props.class}-results-${id}`,
+						onClickPassive: inputSelected,
+						onKeydownPassive: keyHandler,
+						onInputPassive: searchInputHandler,
+						onBlurPassive: () => props.blur && blurHandler,
+						onMouseOverOnce: () => props.instant && autoSuggest,
 					}),
+					// clear button
 					h(
 						'button',
 						{
@@ -69,6 +223,7 @@ export const Suggestify = defineComponent({
 						},
 						slots.delete ? slots.delete() : icon
 					),
+					// submit button
 					h(
 						'button',
 						{
@@ -79,6 +234,7 @@ export const Suggestify = defineComponent({
 						},
 						slots.submit ? slots.submit() : icon
 					),
+					// result list
 					h(
 						'ul',
 						{
@@ -86,7 +242,10 @@ export const Suggestify = defineComponent({
 							class: `${props.class}-results`,
 							role: 'listbox',
 						},
-						results.length && results
+						() =>
+							results.value && results.value.length
+								? results.value.map((result) => (slots.item ? slots.item(result) : item(result)))
+								: null
 					),
 				]
 			);
