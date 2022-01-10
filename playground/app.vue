cc044fd3f9678a964e83c47708e517e2c414f2ac
Max van der Schee <26795741+mvdschee@users.noreply.github.com>
2022-01-10 15:12:33 +0800

    added more search logic

--- playground/app.vue
+++ playground/app.vue
@@ -1,9 +1,23 @@
 <template>
-    <SuggestifySearch class="suggestify" :content="t" />
+    <Search
+        class="suggestify"
+        :content="t"
+        engine="https://suggestify.maxvanderschee.nl/api/suggestions"
+        event="https://suggestify.maxvanderschee.nl/api/events"
+        :blur="false"
+    />
 </template>
 
 <script setup lang="ts">
+import Search from '../src/components/Search.vue';
+
 const t: any = {
     placeholder: 'Search for a movie',
 };
 </script>
+
+<style>
+body {
+    background-color: whitesmoke;
+}
+</style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,11 +1,12 @@
 <template>
-    <div role="search" :class="name">
+    <div role="search" :class="[name, { expanded }]">
         <input
             :placeholder="content.placeholder"
             :aria-label="content.placeholder"
             :class="`${name}-input`"
             autocomplete="off"
             autocapitalize="none"
+            v-model="searchInput"
             autocorrect="off"
             spellcheck="false"
             role="combobox"
@@ -13,26 +14,43 @@
             aria-haspopup="listbox"
             :aria-expanded="expanded"
             :aria-owns="`${name}-results-${id}`"
+            @click="inputSelected"
+            @input="searchInputHandler"
+            @blur="blurHandler"
+            @keydown="keyHandler"
         />
-        <button :aria-label="content.buttonCancel" :class="`${name}-clear`" :hidden="!expanded">
-            <template #delete>
+        <button
+            :aria-label="content.buttonCancel"
+            :class="`${name}-clear`"
+            :hidden="!expanded || !searchInput"
+            @click="resetHandler"
+        >
+            <slot name="delete">
                 <i :class="`${name}-icon`" role="presentation" focusable="false" aria-hidden="true"></i>
-            </template>
+            </slot>
         </button>
-        <button type="submit" :aria-label="content.buttonSubmit" class="suggestify-submit">
-            <template #submit>
+        <button type="submit" :aria-label="content.buttonSubmit" class="suggestify-submit" @click="directSearch">
+            <slot name="submit">
                 <i :class="`${name}-icon`" role="presentation" focusable="false" aria-hidden="true"></i>
-            </template>
+            </slot>
         </button>
-        <ul :id="`${name}-results-${id}`" :class="`${name}-results`" role="listbox"></ul>
+        <ul v-show="results" :id="`${name}-results-${id}`" :class="`${name}-results`" role="listbox">
+            <template v-if="results">
+                <li v-if="content[results.type]" :class="`${name}-banner`">{{ content[results.type] }}</li>
+                <li v-for="(item, index) in results.items" :key="index" :title="item">
+                    <a :href="`${url}${item}`" :class="`${name}-link`" v-html="SearchResultsDisplay(item)"></a>
+                </li>
+            </template>
+        </ul>
     </div>
 </template>
 
 <script setup lang="ts">
-import { ref, Ref, PropType } from 'vue';
+import { ref, Ref, PropType, computed } from 'vue';
 import { nanoid } from 'nanoid';
 import { $fetch } from 'ohmyfetch';
 import { Cache, Content, Result } from '../types';
+import { sanitize } from '../utils';
 import { switchFn } from '../utils';
 
 const props = defineProps({
@@ -44,40 +62,61 @@ const props = defineProps({
         type: Object as PropType<Content>,
         default: () => ({}),
     },
+    engine: {
+        type: String,
+    },
+    event: {
+        type: String,
+    },
+    url: {
+        type: String,
+        default: '?q=',
+    },
+    blur: {
+        type: Boolean,
+        default: true,
+    },
 });
+
 const id = nanoid(6);
-const timeout: number | any = 250;
+let timeout: number | any = 250;
+
 const contentDefault = {
     placeholder: 'Search...',
     buttonSubmit: 'Search',
     buttonCancel: 'Delete input',
+    suggestions: 'Suggestions',
+    empty: 'No suggestions found',
 };
+
 const content = computed(() => ({
     ...contentDefault,
     ...props.content,
 }));
 
-// const input = ref();
-
-const selectedIndex = ref(-1);
 const searchInput = ref('');
-const cache: Ref<Cache> = ref({});
+const cache: Cache = {};
 const expanded = ref(false);
-const results: Ref<Result[]> = ref([]);
+const results: Ref<Result> = ref(null);
+
+// ----------------
+// Handlers
+// ----------------
 
 const request = async (search: string | null): Promise<Result> => {
     const query = search ? search : null;
     const cacheKey = JSON.stringify(query);
-    if (cache.value[cacheKey]) return cache.value[cacheKey];
+    if (cache[cacheKey]) return cache[cacheKey];
 
     try {
-        const { data } = await $fetch(`${props.engine}`, {
+        const data = await $fetch(`${props.engine}`, {
             params: {
                 q: query,
             },
         });
 
-        cache.value[cacheKey] = data;
+        cache[cacheKey] = data;
+
         return data;
     } catch (error: any) {
         throw new Error(error);
@@ -87,9 +126,9 @@ const request = async (search: string | null): Promise<Result> => {
 const keyHandler = ({ key }: KeyboardEvent): void => {
     const cases = {
         Enter: directSearch,
-        Escape: deleteResultList,
-        ArrowUp: selectItemUp,
-        ArrowDown: selectItemDown,
+        Escape: resetHandler,
+        // ArrowUp: selectItemUp,
+        // ArrowDown: selectItemDown,
         _default: () => null,
     };
     const keySwitch = switchFn(cases, '_default');
@@ -97,29 +136,85 @@ const keyHandler = ({ key }: KeyboardEvent): void => {
     keySwitch(key);
 };
 
-const clearInput = (): void => {
-    searchInput.value = '';
-    input.value = '';
+const cleanInput = () => {
+    const input = searchInput.value.trim().toLowerCase();
 
-    deleteResultList();
+    return input ? sanitize(input) : '';
 };
 
 const blurHandler = (): void => {
-    setTimeout(() => {
-        deleteResultList();
-    }, 100);
+    if (props.blur) {
+        setTimeout(() => {
+            resetHandler();
+        }, 100);
+    }
+};
+const resetHandler = (): void => {
+    searchInput.value = '';
+    results.value = null;
+    expanded.value = false;
 };
 
 const inputSelected = async (): Promise<void> => {
-    try {
-        const response = await request(searchInput.value);
+    const search = cleanInput();
 
-        deleteResultList();
-        createResultList(response);
-    } catch (error: any) {
+    try {
+        const response = await request(search);
+
+        results.value = response;
+        expanded.value = true;
+    } catch (error: Error) {
         throw new Error(error);
     }
 };
+
+const directSearch = (): void => {
+    const search = cleanInput();
+
+    console.log('test');
+
+    if (search) {
+        const searchInResults = results.value.items.find((item) => item === search);
+        const success = searchInResults ? 'HIT' : 'MISS';
+
+        if (props.event) navigator.sendBeacon(props.event, JSON.stringify({ value: search, success }));
+
+        window.location.href = `${this.url}${result}`;
+    }
+};
+
+const searchInputHandler = (): void => {
+    if (timeout) clearTimeout(timeout);
+    timeout = setTimeout(async () => {
+        const search = cleanInput();
+
+        if (props.engine)
+            try {
+                const response = await request(search);
+
+                results.value = response;
+                expanded.value = true;
+            } catch (error: Error) {
+                throw new Error(error);
+            }
+    }, 250);
+};
+
+const SearchResultsDisplay = (item: string) => {
+    if (results.value.type === 'results') {
+        const words = searchInput.value ? searchInput.value.split(' ') : [];
+        let text = item;
+
+        for (let i = 0; i < words.length; i++) {
+            const word = words[i];
+            text = text.replace(word, `<b>${word}</b>`);
+        }
+
+        return text;
+    } else return item;
+};
+
+const onComplete = () => {};
 </script>
 
 <style scoped lang="scss"></style>
